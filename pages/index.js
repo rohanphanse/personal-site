19446ceb70c8f533fe4e446b22966794d627ca9c
Rohan Phanse <rohan.a.phanse@gmail.com>
2022-06-07 16:45:02 -0700

    Escape apostrophe with &apos;

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = () => {
                 <title>Rohan Phanse</title>
                 <link rel = "icon" type = "image/png" href = "/images/logo.png" />
             </Head>
-            <h1>Hi I'm Rohan.</h1>
+            <h1>Hi I&apos;m Rohan.</h1>
             <p>I'm passionate about CS and I'm currently learning Rust and smart contracts.</p>
         </>
     )
