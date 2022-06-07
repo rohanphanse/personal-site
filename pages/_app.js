133d340e1159df8d623c41ba5f275fc097c80168
Rohan Phanse <rohan.a.phanse@gmail.com>
2022-06-07 16:41:52 -0700

    Delete boilerplate, add logo.png

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/global.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+    return (
+        <Component {...pageProps} />
+    )
 }
 
-export default MyApp
+export default App
